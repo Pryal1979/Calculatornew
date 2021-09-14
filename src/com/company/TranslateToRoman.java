e4dba0dc5733de7043d0ee348958067c6ff12ffd
Pryal1979 <90647814+Pryal1979@users.noreply.github.com>
2021-09-15 00:33:21 +0300

    Update TranslateToRoman.java

--- src/com/company/TranslateToRoman.java
+++ src/com/company/TranslateToRoman.java
@@ -6,13 +6,17 @@ public class TranslateToRoman {
         x=73;
         System.out.println(Roman(x));
     }
-    public static String Roman(int s)
+ public static String Roman(int s)
     {
         String[] Roman10={"I","II","III","IV","V","VI","VII","VIII","IX","X"},
                 Roman100={"X","XX","XXX","XL","L","LX","LXX","LXXXX","XC","C"};
-        String Otvet="0";
-        if (s<=10) {Otvet=Roman10[s-1];}
-        else if (s>=11) {Otvet=Roman100[((s/10)-1)]+Roman10[s-1-((s/10)*10)];}
+        String Otvet="Ответ = ";
+        if (s<0) Otvet="Недопустимая операция с римскими числами";
+        else if (s==0) Otvet=Otvet+"0";
+        else if ((s>0) && (s<=10)) Otvet=Otvet+Roman10[s-1];
+        else if ((s>=11) && ((s%10)>=1)) Otvet=Otvet+Roman100[(s/10)-1]+Roman10[(s%10)-1];
+        else if ((s>=11) && ((s%10)==0)) Otvet=Otvet+Roman100[(s/10)-1];
+
         return Otvet;
     }
 }
